38a32786e6deba9dc42aa59447723f432d10c258
aimee <akmreiss@yahoo.com>
2015-09-21 19:45:23 -0700

    Add constructors and prototypes:

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
+function Contact(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = [];
+}
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
 
 $(document).ready(function() {
   $("#add-address").click(function() {
@@ -26,7 +44,7 @@ $(document).ready(function() {
   var inputtedFirstName = $("input#new-first-name").val();
   var inputtedLastName = $("input#new-last-name").val();
 
-  var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+  var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
   $(".new-address").each(function() {
     var inputtedStreet = $(this).find("input.new-street").val();
@@ -38,7 +56,7 @@ $(document).ready(function() {
   });
 
 
-  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
   $(".contact").last().click(function() {
     $("#show-contact").show();
@@ -49,9 +67,10 @@ $(document).ready(function() {
 
     $("ul#addresses").text("");
     newContact.addresses.forEach(function(address) {
-      $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+      $("ul#addresses").append("<li>" + address.fullAddress()  + "</li>");
     });
   });
+
 
   $("input#new-first-name").val("");
   $("input#new-last-name").val("");
