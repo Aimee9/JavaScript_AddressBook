4a6ad9e2f3e1d3b0e2d7a62a54eecb202357d97e
aimee reiss & momo ozawa <student@epicodus.com>
2015-09-22 09:48:31 -0700

    Add category to address constructor and refactor resetting form fields

---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+/**
+ * CONTACT
+ */
+
+function Contact(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = [];
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+/**
+ * ADDRESS
+ */
+
+function Address(street, city, state, category) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.category = category;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state + ", " + this.category;
+}
+
+/**
+ * FORM HELPER FUNCTIONS
+ */
+
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+  $("input.new-category").val("");
+}
+
+$(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-category">Category</label>' +
+                                   '<input type="text" class="form-control new-category">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+
+
+  $("form#new-contact").submit(function(event) {
+  event.preventDefault();
+
+  var inputtedFirstName = $("input#new-first-name").val();
+  var inputtedLastName = $("input#new-last-name").val();
+
+  var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+  $(".new-address").each(function() {
+    var inputtedStreet = $(this).find("input.new-street").val();
+    var inputtedCity = $(this).find("input.new-city").val();
+    var inputtedState = $(this).find("input.new-state").val();
+    var inputtedCategory = $(this).find("input.new-category").val();
+
+    var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedCategory);
+    newContact.addresses.push(newAddress);
+  });
+
+
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+  $(".contact").last().click(function() {
+    $("#show-contact").show();
+
+    $("#show-contact h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+
+    $("ul#addresses").text("");
+    newContact.addresses.forEach(function(address) {
+      $("ul#addresses").append("<li>" + address.fullAddress()  + "</li>");
+    });
+  });
+
+  resetFields();
+}); // end of new-contact submit
+
+}); // end of document
